cd43042309ec202081039633a85e416cc2d4375e
vicky <dogovicky05@gmail.com>
2025-02-27 18:12:28 +0300

    Spring Boot initialization

---
+++ Backend-SpringBoot/src/main/java/com/capricon/Collab_Project/CollabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.capricon.Collab_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabProjectApplication.class, args);
+	}
+
+}
---
+++ Backend-SpringBoot/src/test/java/com/capricon/Collab_Project/CollabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capricon.Collab_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
