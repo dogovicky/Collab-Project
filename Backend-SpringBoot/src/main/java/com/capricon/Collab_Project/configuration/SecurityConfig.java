c4035839a6ea516255cd46926f810d9459bfa30f
vicky <victorhikah@gmail.com>
2025-04-09 22:13:52 +0300

    Working on CORS error with deployed front-end

--- Backend-SpringBoot/src/main/java/com/capricon/Collab_Project/configuration/SecurityConfig.java
+++ Backend-SpringBoot/src/main/java/com/capricon/Collab_Project/configuration/SecurityConfig.java
@@ -61,7 +61,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedOriginPattern("https://nexus-frontend-bice.vercel.app");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
