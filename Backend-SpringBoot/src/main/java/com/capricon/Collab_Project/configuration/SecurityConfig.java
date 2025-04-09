7fc146aa36f0cc77e2331e6b4eedcdd8d9325478
vicky <dogovicky05@gmail.com>
2025-04-09 20:11:48 +0300

    Working on CORS error

--- Backend-SpringBoot/src/main/java/com/capricon/Collab_Project/configuration/SecurityConfig.java
+++ Backend-SpringBoot/src/main/java/com/capricon/Collab_Project/configuration/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.capricon.Collab_Project.configuration;
 
 import com.capricon.Collab_Project.filter.JwtFilter;
 import com.capricon.Collab_Project.service.MyUserDetailsService;
-import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,16 +14,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
